751f8c52d5fb1f224eb6ea96cb67ce423f2d31b9
dafneMena <dafneg2306@icloud.com>
2022-09-20 16:59:16 -0700

    Flix part 2 updated

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Dafne Gonzalez on 9/19/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
